d257314ff08fead12c6041fb7d228177b28080bc
Quinton Schafer <quinton@Quintons-MacBook-Pro.local>
2025-05-07 15:01:49 -0400

    removeElement

--- JavaScript/MergeSortedArray.js
+++ JavaScript/MergeSortedArray.js
@@ -8,7 +8,7 @@
 
 // Time complexity: O(m+n)
 var merge = function(nums1, m, nums2, n) {
-let midx = m - 1;
+    let midx = m - 1;
     let nidx = n - 1;
     let right = m + n - 1;
 
@@ -25,7 +25,7 @@ let midx = m - 1;
 };
 
 // Time complexity: O(m+n)
-var merge = function(nums1, m, nums2, n) {
+var merge1 = function(nums1, m, nums2, n) {
     let i = m - 1;
     let j = n - 1;
     let k = m + n - 1;
